d6f647ec8574461bbbf3e21117dfd0e4dd5e66cc
mao <0705.pen@gmail.com>
2024-02-20 21:56:44 +0900

    02.20

---
+++ advance-laravel/src/app/Http/Controllers/BookController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    public function index(Request $request){
+        $items = Book::all();
+        foreach ($items as $item){
+            $item->author;
+        }
+        return view('book.index', ['items'=>$items]);
+    }
+
+
+    public function add(){
+        return view('book.add');
+    }
+    public function create(Request $request){
+        $this->validate($request, Book::$rules);
+        $form = $request->all();
+        Book::create($form);
+        return redirect('/book');
+    }
+}
+
+
---
+++ advance-laravel/src/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use \App\Models\Person;
+
+class DatabaseSeeder extends Seeder
+{
+/**
+* Seed the application's database.
+*
+* @return void
+*/
+  public function run()
+  {
+    Person::factory(10)->create();
+  }
+}
